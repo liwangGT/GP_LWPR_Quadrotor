b42327daee38a42fff8b992c04c66d2cb2556690
kgslovak <kgslovak@gmail.com>
2017-10-25 12:58:21 -0400

    add LWPR test case

---
+++ LWPR/LWPR-tut.py
@@ -0,0 +1,50 @@
+from numpy import *
+from matplotlib import pyplot as plt
+from lwpr import LWPR
+ 
+def testfunc(x):
+    return 10*sin(7.8*log(1+x)) / (1 + 0.1*x**2)
+ 
+ 
+Ntr = 500
+Xtr = 10 * random.random((Ntr, 1))
+Ytr = 5 + testfunc(Xtr) + 0.1 * random.normal(0, 1, (Ntr, 1)) * Xtr
+ 
+# initialize the LWPR model
+model = LWPR(1, 1)     
+model.init_D = 100 * eye(1)
+model.update_D = True
+model.init_alpha = 40 * eye(1)
+model.meta = True
+ 
+# train the model
+for k in range(20):
+    ind = random.permutation(Ntr)
+    mse = 0
+     
+    for i in range(Ntr):
+        yp = model.update(Xtr[ind[i]], Ytr[ind[i]])
+        mse = mse + (Ytr[ind[i], :] - yp)**2
+ 
+    nMSE = mse/Ntr/var(Ytr)
+    print "#Data: %5i  #RFs: %3i  nMSE=%5.3f"%(model.n_data, model.num_rfs, nMSE)
+             
+             
+# test the model with unseen data   
+Ntest = 5000
+Xtest = linspace(0, 10.5, Ntest)
+ 
+Ytest = zeros((Ntest,1))
+Conf = zeros((Ntest,1))
+ 
+for k in range(Ntest):
+    Ytest[k,:], Conf[k,:] = model.predict_conf(array([Xtest[k]]))
+ 
+plt.plot(Xtr, Ytr, 'r.')
+     
+plt.plot(Xtest,Ytest,'b-')
+plt.plot(Xtest,Ytest+Conf,'c-', linewidth=2)
+plt.plot(Xtest,Ytest-Conf,'c-', linewidth=2)
+ 
+plt.show()
+
