085a56fff95851c4a9911b9feec9a86b009bf7fb
liwangGT <wangliinhust@gmail.com>
2017-11-17 11:52:18 -0500

    added ARD kernal GP model

--- SimCompare.py
+++ SimCompare.py
@@ -37,13 +37,40 @@ if __name__ == '__main__':
     """
     Method 0: full GP
     """
-    
+    # get optimal parameters using GPy
+    kernel0 = GPy.kern.RBF(input_dim=9, variance=1., lengthscale=1., ARD=True)
+    m0 = GPy.models.GPRegression(xhist,yhist[:,0][:,None],kernel0)
+    m0.optimize()
 
+    kernel1 = GPy.kern.RBF(input_dim=9, variance=1., lengthscale=1., ARD=True)
+    m1 = GPy.models.GPRegression(xhist,yhist[:,1][:,None],kernel1)
+    m1.optimize()
+
+    kernel2 = GPy.kern.RBF(input_dim=9, variance=1., lengthscale=1., ARD=True)
+    m2 = GPy.models.GPRegression(xhist,yhist[:,2][:,None],kernel2)
+    m2.optimize()
+
+
+    print kernel0
+    print kernel0.lengthscale
+    print kernel1
+    print kernel1.lengthscale
+    print kernel2
+    print kernel2.lengthscale
+
+    #print m
+    #print m.rbf.gradient
+    #m.optimize()
+    #print m.rbf.gradient
+    #print m.rbf.variance
+    #print m.rbf.lengthscale
+    #print m.Gaussian_noise.variance
 
 
     """
     Method 1: recursive GP
     """
+    yM0 = np.empty((0,3))
     yM1 = np.empty((0,3))
     xnew = xhist[0,:]
     ynew = yhist[0,:]
@@ -57,35 +84,35 @@ if __name__ == '__main__':
         ynew = yhist[i,:]
         gpR.Update(xnew, ynew)
 
-        # store predictions for comparision
+        # store predictions for comparision        
         yM1 = np.vstack((yM1, ypred))        
-    
+
+        # full GP prediction
+        ym0,yV0 = m0.predict(xnew[None,:])
+        ym1,yV1 = m1.predict(xnew[None,:])
+        ym2,yV2 = m2.predict(xnew[None,:])
+        print ypred
+        ypred = np.array([ym0, ym1, ym2]).reshape((3,))
+        print ypred
+        yM0 = np.vstack((yM0, ypred))
+
     # visualize data
     # Two subplots, the axes array is 1-d
     f, axarr = plt.subplots(3, sharex=True)
     axarr[0].plot(range(0,yhist.shape[0]), yhist[:,0], 'g-')
     axarr[0].plot(range(0,yhist.shape[0]), yreal[:,0], 'r--')
-    axarr[0].plot(range(0,yM1.shape[0]), yM1[:,0], 'b*')
+    axarr[0].plot(range(0,yM0.shape[0]), yM0[:,0], 'b*')
 
     axarr[1].plot(range(0,yhist.shape[0]), yhist[:,1], 'g-')
     axarr[1].plot(range(0,yhist.shape[0]), yreal[:,1], 'r--')
-    axarr[1].plot(range(0,yM1.shape[0]), yM1[:,1], 'b*')
+    axarr[1].plot(range(0,yM0.shape[0]), yM0[:,1], 'b*')
 
     axarr[2].plot(range(0,yhist.shape[0]), yhist[:,2], 'g-')
     axarr[2].plot(range(0,yhist.shape[0]), yreal[:,2], 'r--')
-    axarr[2].plot(range(0,yM1.shape[0]), yM1[:,2], 'b*')
+    axarr[2].plot(range(0,yM0.shape[0]), yM0[:,2], 'b*')
 
 
 
-    # get optimal parameters using GPy
-    kernel = GPy.kern.RBF(input_dim=9, variance=1., lengthscale=1.)
-    m = GPy.models.GPRegression(xhist,yhist[:,0][:,None],kernel)
-    print m
-    print m.rbf.gradient
-    m.optimize()
-    print m.rbf.gradient
-    print m.rbf.variance
-    print m.rbf.lengthscale
-    print m.Gaussian_noise.variance
+
    
     plt.show()
