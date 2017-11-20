33704b48674031d089e72844a7bd98b91370dcbe
liwangGT <wangliinhust@gmail.com>
2017-11-20 17:51:23 -0500

    added example for SSGP with uncertainty

--- SimCompare.py
+++ SimCompare.py
@@ -21,6 +21,7 @@ import pickle #save multiple objects
 
 #from geometry_msgs.msg import Twist
 import Recursive_GP.GP_pred as gp_cf
+from SSGP_Uncertainty.ssgpy import SSGPy as SSGP  # change if ssgpy.so is in another directory
 
 
 if __name__ == '__main__':
@@ -34,6 +35,9 @@ if __name__ == '__main__':
     tN = len(xhist[:,0])
     t = np.linspace(0, dt*(tN-1), tN)
     print "Number of data points is: ", tN
+
+
+
 
     """
     Method 0: full GP
@@ -73,6 +77,7 @@ if __name__ == '__main__':
     #print m.Gaussian_noise.variance
 
 
+
     """
     Method 1: recursive GP
     """
@@ -102,21 +107,79 @@ if __name__ == '__main__':
         print ypred
         yM0 = np.vstack((yM0, ypred))
 
+    """
+    Method 3: SSGP with uncertainty
+    """
+    # n is x dimension, k is y dimension, D is number of frequencies, ell is 
+    D = 25
+    sf20 = kernel0.rbf.variance
+    ell0 = kernel0.rbf.lengthscale
+    sn20 = kernel0.white.variance
+    ssgp0 = SSGP(9, 1, D, ell0, sf20, sn20)
+
+    sf21 = kernel1.rbf.variance
+    ell1 = kernel1.rbf.lengthscale
+    sn21 = kernel1.white.variance
+    ssgp1 = SSGP(9, 1, D, ell1, sf21, sn21)
+
+    sf22 = kernel2.rbf.variance
+    ell2 = kernel2.rbf.lengthscale
+    sn22 = kernel2.white.variance
+    ssgp2 = SSGP(9, 1, D, ell2, sf22, sn22)
+
+
+    start_update = time.time()
+    ssgp0.update(xhist, yhist[:,0][:,None])
+    ssgp1.update(xhist, yhist[:,1][:,None])
+    ssgp2.update(xhist, yhist[:,2][:,None])
+    end_update = time.time()
+
+    start_pred = time.time()
+    pred0 = ssgp0.predict_mean(xhist)
+    pred1 = ssgp1.predict_mean(xhist)
+    pred2 = ssgp2.predict_mean(xhist)
+    yM3 = np.hstack((pred0[:,None], pred1[:,None], pred2[:,None]))
+    end_pred = time.time()
+
+    """
+    plt.plot(range(len(pred)), pred, 'bo')
+    plt.plot(range(len(yhist[:,0])), yhist[:,0], 'ro')
+    fullgp = matplotlib.patches.Patch(color='green', label='Full GP')
+    sparsegp = matplotlib.patches.Patch(color='blue', label='SSGP')
+    actual = matplotlib.patches.Patch(color='red', label='Actual')
+    plt.legend([fullgp, sparsegp, actual], ['Full GP', 'SSGP', 'Actual'])
+    plt.show()
+
+    print "{0} ms to perform {1} updates".format((end_update - start_update) *
+                                                 1000, Ntrh)
+    print "{0} ms per update".format((end_update - start_update) * 1000 / Ntrh)
+
+    print "{0} ms to perform {1} predictions".format((end_pred - start_pred) *
+                                                     1000, Nts)
+    print "{0} ms per prediction".format((end_pred - start_pred) * 1000 / Nts)
+    """
+
+
+
+
+
     # visualize data
     # Two subplots, the axes array is 1-d
     f, axarr = plt.subplots(3, sharex=True)
     axarr[0].plot(range(0,yhist.shape[0]), yhist[:,0], 'g-')
     axarr[0].plot(range(0,yhist.shape[0]), yreal[:,0], 'r--')
     axarr[0].plot(range(1,yM0.shape[0]+1), yM0[:,0], 'b*')
+    axarr[0].plot(range(0,yM3.shape[0]), yM3[:,0], 'k+')
 
     axarr[1].plot(range(0,yhist.shape[0]), yhist[:,1], 'g-')
     axarr[1].plot(range(0,yhist.shape[0]), yreal[:,1], 'r--')
     axarr[1].plot(range(1,yM0.shape[0]+1), yM0[:,1], 'b*')
+    axarr[1].plot(range(0,yM3.shape[0]), yM3[:,1], 'k+')
 
     axarr[2].plot(range(0,yhist.shape[0]), yhist[:,2], 'g-')
     axarr[2].plot(range(0,yhist.shape[0]), yreal[:,2], 'r--')
     axarr[2].plot(range(1,yM0.shape[0]+1), yM0[:,2], 'b*')
-
+    axarr[2].plot(range(0,yM3.shape[0]), yM3[:,2], 'k+')
 
 
 
